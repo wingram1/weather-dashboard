8ff1a4d60066d45276cef4d1af6600b0250e6869
wingram1 <waingram96@gmail.com>
2022-02-08 18:44:01 -0600

    search history generated in reverse order

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 //misc global variables
 var searchForm = document.querySelector("#search-container");
+var historyContainer = document.querySelector("#history-container");
 var citySearchHistory = [];
 
 // take input, save to localStorage
@@ -13,19 +14,23 @@ var formSubmitHandler = function(event) {
     if (citySearchHistory.length > 0) {
 
         // if there is, check citySearchHistory for an object with name equal to submission
-        for (i=0; i<citySearchHistory.length; i++) {
+        for (i=0; i< (citySearchHistory.length + 1); i++) {
 
-            // if search input matches a city, break the loop
-            if (citySearchHistory[i].name === searchInput) {
-                console.log(citySearchHistory[i].name + " === " + searchInput + ". Saving to localStorage.");
+            // if search input matches a city, splice then re-save
+            if (citySearchHistory[i] === searchInput) {
+                console.log(citySearchHistory[i] + " === " + searchInput + ". Saving to localStorage.");
+                citySearchHistory.splice(i, 1);
+                saveCity(searchInput);
                 break;
             }
             // if there isn't, log that cities were compared and keep looping
-            else if (citySearchHistory[i].name != searchInput) {
-                console.log(citySearchHistory[i].name + " != " + searchInput);
+            else if (citySearchHistory[i] && citySearchHistory[i] != searchInput) {
+                console.log(citySearchHistory[i] + " != " + searchInput);
             }
-            else {
-                console.log("Error looping through history")
+            // if nothing on the last iteration, save a new one
+            else if (!citySearchHistory[i]) {
+                console.log("New city saved: " + searchInput);
+                saveCity(searchInput);
                 break;
             }
         } 
@@ -49,6 +54,9 @@ var saveCity = function(newCity) {
     console.log(citySearchHistory);
 
     localStorage.setItem("Cities", JSON.stringify(citySearchHistory));
+
+    // update search history
+    loadCities();
 };
 
 // function to load search history
@@ -61,14 +69,25 @@ var loadCities = function() {
     if (storedCities != null) {
         console.log("You've got cities!");
         citySearchHistory = JSON.parse(storedCities);
-        // generateHistory();
+
+        // kill all babies of historyContainer
+        $("#history-container").empty();
+
+
+        // make new babies
+        for (i = (citySearchHistory.length - 1); i > -1; i--) {
+            var workingButton = document.createElement("button");
+            workingButton.className = "btn btn-secondary mt-2 mb-2";
+            workingButton.textContent = citySearchHistory[i];
+            historyContainer.appendChild(workingButton);
+        }
+        console.log("Finished loading history.")
     }
     else {
         console.log("No cities found.");
         citySearchHistory = [];
     }
 }
-
 
 // fetch weather from API
 
